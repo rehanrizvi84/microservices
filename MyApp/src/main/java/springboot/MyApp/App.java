ee61660f45b422712930c57bad11e3a89c0fbf81
rehanrizvi84 <rehanrizvi84@yahoo.in>
2019-02-04 21:16:53 -0800

    New project

---
+++ MyApp/src/main/java/springboot/MyApp/App.java
@@ -0,0 +1,13 @@
+package springboot.MyApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
